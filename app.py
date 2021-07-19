1feb77111a0e881d564bde35fbbb4990fb24513c
piet18cs127 <83266550+piet18cs127@users.noreply.github.com>
2021-07-19 13:21:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st 
 from PIL import Image
 import pickle
 import numpy as np
@@ -13,24 +13,24 @@ from keras.models import load_model
 
 html_temp = """
    <div class="" style="background-color:blue;" >
-   <div class="clearfix">
+   <div class="clearfix">           
    <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center>
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Digital Image Processing lab</p></center>
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Digital Image Processing lab by Rahul Kumar Agrawal</p></center> 
    </div>
    </div>
    </div>
    """
 st.markdown(html_temp,unsafe_allow_html=True)
-
+  
 st.title("""
         Various shades of RED color
          """
          )
 file= st.file_uploader("Please upload image", type=("jpg", "png"))
 R = st.slider('R', min_value=0, max_value=255, step=1)
-G = st.slider('G', min_value=0, max_value=255, step=1)
-B = st.slider('B', min_value=0, max_value=255, step=1)
+# G = st.slider('G', min_value=0, max_value=255, step=1)
+# B = st.slider('B', min_value=0, max_value=255, step=1)
 
 import cv2
 from  PIL import Image, ImageOps
@@ -39,7 +39,7 @@ def import_and_predict(image_data):
   #image = image.img_to_array(img)
   #img_reshap= np.expand_dims(image, axis=0)
   #img_reshap = preprocess_input(img_reshap)
-
+   
   image_data[:] = [R,0,0]
   st.image(image_data, use_column_width=True)
   return 0
@@ -49,18 +49,18 @@ else:
   file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
   image = cv2.imdecode(file_bytes, 1)
   st.image(file,caption='Uploaded Image.', use_column_width=True)
-
+    
 if st.button("Change Color"):
   result=import_and_predict(image)
-
+  
 if st.button("About"):
   st.header("Developed by Sagar Gyanchandani")
-  st.subheader("PIET18CS127, Student, Department of Computer Engineering")
+  st.subheader("PIET18CS127")
 html_temp = """
-   <div class="" style="background-color:orange;" >
-   <div class="clearfix">
+   <div class="" style="background-color:black;" >
+   <div class="clearfix">           
    <div class="col-md-12">
-   <center><p style="font-size:20px;color:white;margin-top:10px;">Digital Image processing Midterm 2</p></center>
+   <center><p style="font-size:20px;color:white;margin-top:10px;">Digital Image processing RTU Final Practical</p></center> 
    </div>
    </div>
    </div>
